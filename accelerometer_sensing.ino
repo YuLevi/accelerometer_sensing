bca91b528a5bf796340f5237855dc535dc981e7f
Levi Y <81494037+YuLevi@users.noreply.github.com>
2023-04-26 19:24:47 +0100

    Allowed use of two serial streams

--- accelerometer_sensing.ino
+++ accelerometer_sensing.ino
@@ -1,9 +1,9 @@
 #include <DFRobot_WT61PC.h>
 #include <SoftwareSerial.h>
 
-// Create SoftwareSerial object for connected accelerometers
+// Create SoftwareSerial object for connected accelerometers (tx, rx)
 SoftwareSerial accLeft (10,11);
-SoftwareSerial accRight (5,6);
+SoftwareSerial accRight (8,9);
 
 // Create DFRobot_WT61PCs object for accelerometers
 DFRobot_WT61PC sensorLeft(&accLeft);
@@ -16,14 +16,19 @@ void setup (){
   accRight.begin(9600);
 
   // Set frequency of data acquisition
-  sensorLeft.modifyFrequency(FREQUENCY_0_1HZ);
-  sensorRight.modifyFrequency(FREQUENCY_0_1HZ);
+  sensorLeft.modifyFrequency(FREQUENCY_10HZ);
+  sensorRight.modifyFrequency(FREQUENCY_10HZ);
 }
 
 void loop(){
-  if (sensorLeft.available() && sensorRight.available()){
-    sendData(sensorLeft, "left");
-    sendData(sensorRight, "right");
+  accLeft.listen(); // Select accLeft for receiving
+  if (sensorLeft.available()){
+    sendData(sensorLeft, "1");
+  }
+
+  accRight.listen(); // Select accRight for receiving
+  if (sensorRight.available()){
+    sendData(sensorRight, "2");
   }
 }
 
@@ -32,5 +37,5 @@ void sendData(DFRobot_WT61PC sensorData, String ID){
   Serial.println(String(millis())+"\t"+ID);
   Serial.println("Acc\t"+String(sensorData.Acc.X)+"\t"+String(sensorData.Acc.Y)+"\t"+String(sensorData.Acc.Z));
   Serial.println("Gyro\t"+String(sensorData.Gyro.X)+"\t"+String(sensorData.Gyro.Y)+"\t"+String(sensorData.Gyro.Z));
-  Serial.println("Angle\t"+String(sensorData.Angle.X)+"\t"+String(sensorData.Angle.Y)+"\t"+String(sensorRight.Angle.Z));
+  Serial.println("Angle\t"+String(sensorData.Angle.X)+"\t"+String(sensorData.Angle.Y)+"\t"+String(sensorData.Angle.Z));
 }
