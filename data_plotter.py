5828dd5b428a7ec3bcee4683b12b01f946028fe8
Levi Y <81494037+YuLevi@users.noreply.github.com>
2023-04-30 21:48:49 +0100

    Rewrite code to use animation in plotting

--- data_plotter.py
+++ data_plotter.py
@@ -1,64 +1,89 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+from serial_read import SerialHandler
 import numpy as np
-from datetime import datetime
+
+import matplotlib
+matplotlib.use('Qt5Agg')
 
 class RealTimePlotter:
     """
     A real-time 3D plotter for visualising stream data.
     """
 
-    def __init__(self, title=None, xlim=(-10, 10), ylim=(-10, 10), zlim=(-10, 10), marker='o-', color='b'):
-        """
-        Create a new RealTimePlotter object.
+    def __init__(self, title=None, interval=100, sample_size=60, source=None, sensor_id=None, 
+                 ylim=((-20,20),(-1000,1000),(-180,180)), marker='-', color='b'):
+        self.title = title
+        self.interval = interval
+        self.sample_size = sample_size
+        self.source = source
+        self.sensor_id = sensor_id
+        self.marker = marker
+        self.color = color
+        self.ylim = ylim
 
-        Args:
-        - title: str, the title of the plot (default: None)
-        - xlim: tuple or list of length 2, the limits of the x-axis (default: (-10, 10))
-        - ylim: tuple or list of length 2, the limits of the y-axis (default: (-10, 10))
-        - zlim: tuple or list of length 2, the limits of the z-axis (default: (-10, 10))
-        - marker: str, the style of the data markers (default: 'o-')
-        - color: str, the color of the data line (default: 'b')
-        """
-        self.fig = plt.figure(title, figsize=(6,4))
-        self.ax = self.fig.subplots(3,1)
-        self.fig.subplots_adjust(hspace=0.3)
-        for ax in self.ax: 
-            ax.grid(True)
-            ax.set_xlabel("Time (s)")
-            ax.set_xlim(0,60)
-        self.ax[0].set_ylabel("X-axis"), self.ax[1].set_ylabel("Y-axis"), self.ax[2].set_ylabel("Z-axis")
-        self.ax[0].set_ylim(*xlim), self.ax[1].set_ylim(*ylim), self.ax[2].set_ylim(*zlim)
-        self.fig.suptitle(title)
-        self.x, self.y, self.z = [], [], []
-        self.line_x, = self.ax[0].plot(self.x, marker, color=color)
-        self.line_y, = self.ax[1].plot(self.y, marker, color=color)
-        self.line_z, = self.ax[2].plot(self.z, marker, color=color) 
+        self.fig = plt.figure(self.title, figsize=(12,7.5))
+        self.fig.suptitle(self.title)
+        self.ax = self.fig.subplots(3,3)
+        self.fig.subplots_adjust(hspace=0.3, left=0.1, bottom=0.065, top=0.9)
 
-    def update_plot(self, elapsed_time, new_data):
-        x, y, z = new_data
-        self.x.append((elapsed_time, x)), self.y.append((elapsed_time, y)), self.z.append((elapsed_time, z))
-        self.line_x.set_data(*zip(*self.x))
-        self.line_y.set_data(*zip(*self.y))
-        self.line_z.set_data(*zip(*self.z))
+        for i, j in np.ndindex((3, 3)):
+            self.ax[i, j].grid(True)
+            self.ax[i, j].set(xlabel="Time (s)", xlim=(0, self.sample_size), ylim=self.ylim[i])
 
-        for ax, data in zip(self.ax, [self.x, self.y, self.z]):
-            if elapsed_time >= ax.get_xlim()[1] - 1:
-                ax.set_xlim(ax.get_xlim()[0], 2*ax.get_xlim()[1])
-                ax.figure.canvas.draw()
-            min_data, max_data = min(d[1] for d in data), max(d[1] for d in data)
-            data_range = np.ptp([d[1] for d in data])
-            if not data_range: data_range = 1  # Avoid zero range
-            ax.set_ylim(min_data - 0.1 * data_range, max_data + 0.1 * data_range)
-        plt.pause(0.01)
+        self.ax[0,0].set_title('X-axis'), self.ax[0,1].set_title('Y-axis'), self.ax[0,2].set_title('Z-axis')
+        self.ax[0,0].set_ylabel('Acceleration (m/s²)'), self.ax[1,0].set_ylabel('Gyro (deg/s)'), self.ax[2,0].set_ylabel('Angle (deg)')
+        self.fig.suptitle(self.title)
 
-    def __enter__(self):
-        plt.ion()
-        plt.show()
-        return self
+        self.AccX, self.AccY, self.AccZ = [np.empty(0)] * 3
+        self.GyrX, self.GyrY, self.GyrZ = [np.empty(0)] * 3
+        self.AngX, self.AngY, self.AngZ = [np.empty(0)] * 3
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-        plt.savefig(f"../captured_accel_data/realtime_plot_{timestamp_str}.png")    
-        plt.show(block=True)
-        plt.close()
+        self.line_AccX, = self.ax[0,0].plot(self.AccX, self.marker, color=self.color)
+        self.line_AccY, = self.ax[0,1].plot(self.AccY, self.marker, color=self.color)
+        self.line_AccZ, = self.ax[0,2].plot(self.AccZ, self.marker, color=self.color)
+
+        self.line_GyrX, = self.ax[1,0].plot(self.GyrX, self.marker, color=self.color)
+        self.line_GyrY, = self.ax[1,1].plot(self.GyrY, self.marker, color=self.color)
+        self.line_GyrZ, = self.ax[1,2].plot(self.GyrZ, self.marker, color=self.color)
+
+        self.line_AngX, = self.ax[2,0].plot(self.AngX, self.marker, color=self.color)
+        self.line_AngY, = self.ax[2,1].plot(self.AngY, self.marker, color=self.color)
+        self.line_AngZ, = self.ax[2,2].plot(self.AngZ, self.marker, color=self.color)
+
+        self.line = [self.line_AccX, self.line_AccY, self.line_AccZ, 
+                     self.line_GyrX, self.line_GyrY, self.line_GyrZ, 
+                     self.line_AngX, self.line_AngY, self.line_AngZ]
+
+    def animate(self, i):
+        new_data = (self.source.read_data()[self.sensor_id])
+        Acc, Gyr, Ang = [float(x) for x in new_data[2:5]], [float(x) for x in new_data[5:8]], [float(x) for x in new_data[8:11]]
+        time = float(new_data[1])
+
+        self.AccX, self.AccY, self.AccZ = [np.append(x, (time, Acc[i])) for i, x in enumerate([self.AccX, self.AccY, self.AccZ])]
+        self.GyrX, self.GyrY, self.GyrZ = [np.append(x, (time, Gyr[i])) for i, x in enumerate([self.GyrX, self.GyrY, self.GyrZ])]
+        self.AngX, self.AngY, self.AngZ = [np.append(x, (time, Ang[i])) for i, x in enumerate([self.AngX, self.AngY, self.AngZ])]
+
+        data_sets = [(self.AccX, self.line_AccX), (self.AccY, self.line_AccY), (self.AccZ, self.line_AccZ),
+             (self.GyrX, self.line_GyrX), (self.GyrY, self.line_GyrY), (self.GyrZ, self.line_GyrZ),
+             (self.AngX, self.line_AngX), (self.AngY, self.line_AngY), (self.AngZ, self.line_AngZ)]
+
+        for data, line in data_sets:
+            line.set_data(data[::2], data[1::2])
+
+        for ax in self.ax.flatten():
+            if time >= ax.get_xlim()[1] - 1:
+                ax.set_xlim(ax.get_xlim()[1]-30, ax.get_xlim()[1] + self.sample_size)
+                self.fig.canvas.draw_idle()
+
+            min_data, max_data = min(ax.get_lines()[0].get_ydata()), max((ax.get_lines()[0].get_ydata()))
+            
+            if min_data < ax.get_ylim()[0] or max_data > ax.get_ylim()[1]:
+                ax.set_ylim((min_data // 10) * 10, (max_data // 10 + 1) * 10)
+                self.fig.canvas.draw_idle()
+
+        return self.line
+
+    def start(self):
+        self.ani = FuncAnimation(self.fig,self.animate,interval=50,blit=True)
+        return self.ani
