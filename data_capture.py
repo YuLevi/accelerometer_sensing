bb444f3a6aba572758f3a9f9f7f0658532569674
Levi Y <81494037+YuLevi@users.noreply.github.com>
2023-05-01 22:45:39 +0100

    Improved data logging and real-time plotting

--- data_capture.py
+++ data_capture.py
@@ -1,36 +1,49 @@
-import serial
-import serial.tools.list_ports
 import csv
 from datetime import datetime
+from serial_read import SerialHandler
+from data_plotter import RealTimePlotter
+from matplotlib import pyplot as plt
 
-# Auto-find and select Arduino serial port
-arduino_ports = [p.device for p in serial.tools.list_ports.comports() if 'Arduino' in p.description]
-if not arduino_ports:
-    raise IOError("No Arduino found")
+SENSORS_COUNT = 2
+
+class SaveToFile:
+    def __init__(self, filename, CSV_HEADER, save_every=10):
+        self.filename = filename
+        self.headers = CSV_HEADER
+        self.counter = 0
+        self.save_every = save_every
+        self.file = open(self.filename, mode='w', newline='')
+        self.data_file = csv.writer(self.file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        self.data_file.writerow(self.headers)
+
+    def data_save(self, collected_data):
+        self.data_file.writerow(collected_data)
+        self.counter += 1
+        if self.counter % self.save_every == 0:
+            self.file.flush()  # flush buffer to ensure data is written to disk
+
+    def image_save(self):
+        return f"{self.filename[:-3]}.png"
+
+    def close(self):
+        self.file.close()
 
 # File config
-timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-filename = f"../captured_accel_data/accel_data_{timestamp_str}.csv"
-CSV_HEADER = ['SensorID','TimeElapsed', 'AccX', 'AccY', 'AccZ', 'GryoX', 'GryoY', 'GryoZ', 'AngleX', 'AngleY', 'AngleZ']
+timestamp_str = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+CSV_HEADER = ['SensorID','TimeElapsed', 'AccX', 'AccY', 'AccZ', 'GyroX', 'GyroY', 'GyroZ', 'AngleX', 'AngleY', 'AngleZ']
+filenames = [f"../captured_accel_data/accel_data_{timestamp_str}_{i}.csv" for i in range(0, SENSORS_COUNT)]
+sensor_files = [SaveToFile(filename, CSV_HEADER) for filename in filenames]
 
-# Serial port configuration
-with serial.Serial(arduino_ports[0], 115200) as serialRead:
-    # Open CSV file for appending
-    with open(filename, mode='a', newline='') as data_file:
-        data_file = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        data_file.writerow(CSV_HEADER)
+plot_objects = {}
+date_time = datetime.now().strftime("%d/%m/%Y %H:%M")
+data_source = SerialHandler()
+for sensor_num in range(0, SENSORS_COUNT):
+    plot_objects[sensor_num] = RealTimePlotter(title=f"Sensor {sensor_num} - {date_time}", source=data_source, 
+                                               sensor_id=sensor_num, data_file=sensor_files[sensor_num])
 
-        # Read data from serial port (with error handling)
-        while True:
-            try:
-                # Read line from serial port, decode it and write to file
-                line = serialRead.readline().decode().strip()
-                values = line.split(',')
-                data_file.writerow(values)
+ani = plot_objects[0].start(), plot_objects[1].start()
 
-            except serial.SerialException as e:
-                    print(f"Serial port disconnected: {e}")
-                    break
-            
-            except Exception as e:
-                    print(f"Error reading serial port: {e}")
+plot_objects[0].fig.canvas.mpl_connect('close_event', plot_objects[0].on_close)
+plot_objects[1].fig.canvas.mpl_connect('close_event', plot_objects[1].on_close)
+
+plt.show()
